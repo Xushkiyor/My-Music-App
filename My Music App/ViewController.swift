81bd66e03a2906121e65dc6031cae39539edc88e
Xushkiyor <nnosirov99@gmail.com>
2022-11-30 16:13:36 +0500

    Initial Commit

---
+++ My Music App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My Music App
+//
+//  Created by Nosirov Xushkiyor Shavkatbek o'g'li on 30/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
